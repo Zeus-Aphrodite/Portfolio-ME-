636c66ea3db47637ba5d3718ba65a29854b2aa72
Zeus-Aphrodite <zeusa1398@gmail.com>
2024-09-21 18:46:19 +0900

    first commit

---
+++ constants/constants.js
@@ -0,0 +1,83 @@
+export const NAME = 'Toru Ichikawa';
+
+export const DESIGNATION = `Blockchain and Fullstack Developer`;
+
+export const DETAILS = {
+  Residence: 'Japan',
+  City: 'Osaka',
+  Age: '32',
+};
+
+export const SKILLS = [
+  {
+    title: 'Blockchain Developer',
+    level: '90%',
+  },
+  {
+    title: 'Smart Contract Auditor',
+    level: '40%',
+  },
+  {
+    title: 'MERN Stack Developer',
+    level: '80%',
+  },
+  {
+    title: 'Backend Developer',
+    level: '50%',
+  },
+  {
+    title: 'Frontend Developer',
+    level: '80%',
+  },
+  {
+    title: 'App Developer',
+    level: '40%',
+  },
+  // {
+  //   title: 'Technical Blogger',
+  //   level: '99%',
+  // },
+];
+
+export const TECH_STACK = [
+  'ReactJS',
+  // 'JavaScript',
+  'NextJS',
+  'Python',
+  'Web3',
+  'Solidity',
+  // 'Kotlin',
+  // 'Android',
+  'Rust',
+  'C++',
+  // 'JAVA',
+  'TypeScript',
+  'NodeJS',
+  'Redux',
+  'Zustand',
+  'CSS',
+  'SCSS',
+  'TailwindCSS',
+  'Ant Design',
+  'Go',
+  // 'Next UI',
+  'REST API',
+  'MongoDB',
+  'MySQL',
+  'Git',
+  'GitHub',
+  // 'Jira',
+  'Figma',
+  'Adobe XD',
+];
+
+export const CONTACTS = {
+  EMAIL: 'zeusa1398@gmail.com',
+  PHONE: '+080 557 44194',
+};
+export const SOCIAL_LINKS = {
+  GITHUB: 'https://github.com/Zeus-Aphrodite?tab=repositories&q=&type=&language=&sort=',
+  LINKEDIN: '',
+  TWITTER: '',
+  FACEBOOK: '',
+};
